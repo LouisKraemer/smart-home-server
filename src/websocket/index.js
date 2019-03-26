420568cd2d634f82b2ee36476cc5c068a9737201
Louis Kraemer <louisk@bam.tech>
2019-03-26 13:21:03 +0100

    feat(websocket): websocket are now secured by JWT

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -1,8 +1,24 @@
 const WebSocket = require("ws");
 
 const { parseIncomingMessage } = require("./utils");
+const { checkJWT } = require("../user/utils");
+const { UNAUTHORIZED } = require("./constants");
 
-const wss = new WebSocket.Server({ port: process.env.WEBSOCKET_PORT });
+const wss = new WebSocket.Server({
+  port: process.env.WEBSOCKET_PORT,
+  verifyClient: (info, cb) => {
+    const token = info.req.headers.token;
+    if (!token) cb(false, 401, UNAUTHORIZED);
+    else {
+      const isJWTValid = checkJWT(token);
+      if (isJWTValid) {
+        cb(true);
+      } else {
+        cb(false, 401, UNAUTHORIZED);
+      }
+    }
+  }
+});
 
 const initWs = () => {
   wss.on("connection", ws => {
