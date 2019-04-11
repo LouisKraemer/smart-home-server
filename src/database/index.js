be219975b4ceaccff2938689e02412c01d8e6108
Louis Kraemer <louisk@bam.tech>
2019-04-11 18:55:45 +0200

    fix(config): wait for db berfore config

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,30 @@
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
-const initConnection = () =>
-  mongoose
-    .connect(
+const sleep = timeout =>
+  new Promise(resolve => {
+    setTimeout(resolve, 3000);
+  });
+
+const initConnection = async () => {
+  try {
+    console.log("Trying to connect to mongodb");
+    await mongoose.connect(
       `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/smart-db`,
       {
         useNewUrlParser: true,
         autoReconnect: true,
         useFindAndModify: false
       }
-    )
-    .then(() => console.log("Connected to database"))
-    .catch(err => {
-      console.log("Error connecting to database, retrying in 1 second");
-      setTimeout(initConnection, 1000);
-    });
+    );
+    console.log("Connected to database");
+    return;
+  } catch (error) {
+    const timeout = 3000;
+    console.log(`Error connecting to database, retrying in ${timeout} ms`);
+    await sleep(timeout);
+    return initConnection();
+  }
+};
 
 module.exports = { initConnection };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const { configureRooms } = require("./seed/configureRoom");
+const { initPingYeelight } = require("./seed/cron");
 
 const express = require("express");
 const app = express();
@@ -21,9 +22,10 @@ const { initRoutes } = require("./routes");
 initRoutes(app);
 
 init = async () => {
+  await initConnection();
+  await configureRooms();
   await Promise.all([
-    configureRooms(),
-    initConnection(),
+    // initPingYeelight(),
     initWs(),
     discoverYeelight()
   ]);
@@ -33,7 +35,7 @@ init = async () => {
 };
 
 try {
-  init();
+  init().then(() => console.log("ended"));
 } catch (error) {
   console.error("Error starting server", error);
 }
