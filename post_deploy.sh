5349536ab4b717ea1a763c5748ad0b3b28e397f8
Louis Kraemer <louisk@bam.tech>
2019-04-21 16:29:22 +0200

    feat(deploy): added script to deploy

---
+++ post_deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ~/smart-home-server
+
+yarn
+
+pm2 start smart-home-server
---
+++ pre_deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pm2 stop smart-home-server
+
+rm -rf ~/smart-home-server/dist
+rm -rf ~/smart-home-server/package.json
+rm -rf ~/smart-home-server/yarn.lock
