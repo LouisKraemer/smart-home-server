76151631e108b1a365512883cd142d4a919ff029
Louis Kraemer <louisk@bam.tech>
2019-04-21 22:41:10 +0200

    chore(config): empty config

--- config.js
+++ config.js
@@ -1,21 +1,5 @@
-const rooms = [
-  {
-    roomId: "weegle_room",
-    users: [{ pseudo: "Weegle" }],
-    yeelights: [{ name: "Weegle's Room", deviceId: "0x0000000007dd1760" }]
-  }
-  // {
-  //   roomId: "living_room",
-  //   users: [{ pseudo: "Weegle" }],
-  //   yeelights: [{ name: "Living Room", deviceId: "0x000000000458ad30" }]
-  // }
-];
+const rooms = [];
 
-const switches = [
-  {
-    switchId: "0x00158d00020415b8",
-    yeelightId: "0x0000000007dd1760"
-  }
-];
+const switches = [];
 
 module.exports = { rooms, switches };
